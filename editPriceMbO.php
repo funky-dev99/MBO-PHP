4920b07afd5ff722c784b7775733bc63b0c686ff
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-11-13 23:38:11 +0530

    Function Added

---
+++ editPriceMbO.php
@@ -0,0 +1,33 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-type: application/json; charset=utf-8");
+require_once("dbconfigB.php");
+
+$item_code  = $_POST['item_code'];
+$price  = $_POST['price'];
+
+
+$query = "SELECT * FROM items_mbo WHERE item_code='$item_code'";  
+ 
+
+$res = mysqli_query($con,$query);
+$data = mysqli_fetch_array($res);
+
+    if ($data[0] >= 1) {
+        $query = "UPDATE items_mbo SET  
+        price = '$price'
+        WHERE item_code='$item_code'";
+        
+                $res = mysqli_query($con, $query);
+        
+                if ($res) {
+                    echo json_encode("true");
+                } else {
+                    echo json_encode("false");
+                }
+            } else {
+                echo json_encode("false");
+	}
+
+
+?>
